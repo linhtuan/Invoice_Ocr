999ad3814b0639eedea61ab3ef3bd7a96cf13183
linh <linhnlt@pixelz.com>
2016-08-22 00:50:11 +0700

    update code

--- OcrForm/js/scanTwainImage.js
+++ OcrForm/js/scanTwainImage.js
@@ -53,7 +53,7 @@ function OnHttpUploadFailure(errorCode, errorString, sHttpResponse) {
 }
 
 function UploadImage() {
-    $('#dwtcontrolContainer canvas')
+
     if (DWObject) {
         // If no image in buffer, return the function
         if (DWObject.HowManyImagesInBuffer == 0)
